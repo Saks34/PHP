c820fd92525fd2d5504ecb431d48f3b99cd7d774
Saksham <sakshamsatnalika34@gmail.com>
2024-09-13 16:26:59 +0530

    modified:   ai.php

--- ai.php
+++ ai.php
@@ -1,3 +1,16 @@
 <?php
+function swap(&$a, &$b) {
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+}
 
+$x = 5;
+$y = 10;
+
+echo "Before swapping: x = $x, y = $y<br>";
+
+swap($x, $y);
+
+echo "After swapping: x = $x, y = $y";
 ?>
