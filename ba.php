1910dccdfea6202e621563f5faef22f63cb6014f
Saksham <sakshamsatnalika34@gmail.com>
2024-09-27 16:35:26 +0530

    modified:   ba.php 	modified:   form.php

--- ba.php
+++ ba.php
@@ -1,3 +1,19 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form 1</title>
+</head>
+<body>
+    <form method="POST" action="form.php">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name">
+        <br>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
+        <br>
+        <input type="submit" name="submit" id="button" value="Next">
+    </form>
+</body>
+</html>
--- form.php
+++ form.php
@@ -3,22 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form 2</title>
 </head>
 <body>
-    <form method="POST" action ="<?php echo $_SERVER["PHP_SELF"]?>">
-        <label for="name">Name</label>
-        <input type="name" id="name" name="name">
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="ph">Phone Number</label>
+        <input type="number" id="ph" name="ph">
         <br>
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
+        <label for="address">Address</label>
+        <input type="text" id="address" name="address">
         <br>
-        <input type="submit" name ="save" id="button">
+        <input type="hidden" name="name" value="<?php echo $_POST['name']; ?>">
+        <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+        <input type="submit" name="save" id="button" value="Submit">
     </form>
+    
     <?php
-    if(isset($_POST["save"]))
-    {
-        echo "Welcome ", $_POST["name"], "Your Password is ",$_POST["password"];
+    if (isset($_POST["save"])) {
+        echo "Welcome " . $_POST["name"] . ", your email is " . $_POST["email"] . ", your phone number is " . $_POST["ph"] . ", and your address is " . $_POST["address"];
     }
     ?>
 </body>
